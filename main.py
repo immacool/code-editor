194960acf927b40f71e70b940d20e29bc4f561b9
immacool <65336226+immacool@users.noreply.github.com>
2022-09-05 11:46:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Tuple
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
@@ -6,7 +7,54 @@ from PyQt6.Qsci import *
 
 
 class CustomEditor(QsciScintilla):
-    def keyPressEvent(self, e: QKeyEvent):
+    def __init__(self, *args, key_press_handler=None, **kwargs):
+        super(CustomEditor, self).__init__(*args, **kwargs)
+        self.key_press_handler = key_press_handler
+        
+        self.setUtf8(True)
+        
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QColor('gainsboro'))
+        
+        self.setAutoIndent(True)
+        self.setIndentationGuides(False)
+        self.setIndentationsUseTabs(True)
+        self.setIndentationWidth(4)
+        
+        self.setMarginsBackgroundColor(QColor("gainsboro"))
+        self.setMarginLineNumbers(1, True)
+        self.setMarginWidth(1, 50)
+        
+        self.setAutoCompletionSource(QsciScintilla.AutoCompletionSource.AcsAll)
+        self.setAutoCompletionCaseSensitivity(True)
+        self.setAutoCompletionReplaceWord(True);
+        self.setAutoCompletionUseSingle(QsciScintilla.AutoCompletionUseSingle.AcusAlways);
+        self.setAutoCompletionThreshold(0);
+        
+        self.setFolding(QsciScintilla.FoldStyle.BoxedTreeFoldStyle)
+
+        self.setBraceMatching(QsciScintilla.BraceMatch.SloppyBraceMatch);
+        self.setMatchedBraceBackgroundColor(QColor("white"));
+        self.setUnmatchedBraceForegroundColor(QColor("red"));
+        
+    def changeEvent(self, event):
+        """
+        The changeEvent function is called whenever the user changes a text in the editor. 
+        It is used to track whether there are unsaved changes in the code.
+        """
+        self.unsaved_changes = True
+    
+    def keyPressEvent(self, e: QKeyEvent) -> None:
+        """
+        The keyPressEvent function is called whenever the user presses a key. 
+        If the control modifier and spacebar are pressed at the same time, 
+        the autoCompleteFromAll function is called to show all possible completions.
+        
+        :param e:QKeyEvent: Pass the event to the parent class
+        """
+        if self.key_press_handler:
+            self.key_press_handler()
+        
         if e.modifiers() == Qt.KeyboardModifier.ControlModifier and e.key() == Qt.Key.Key_Space:
             self.autoCompleteFromAll()
             return
@@ -18,122 +66,202 @@ class CustomMainWindow(QMainWindow):
     def __init__(self):
         super(CustomMainWindow, self).__init__()
 
-        # Window setup
-        # --------------
-
-        # 1. Define the geometry of the main window
         self.setGeometry(300, 300, 800, 400)
         self.setWindowTitle("Code editor")
+        
+        self.is_new_file = False
+        
+        layout = QVBoxLayout()
 
-        # 2. Create frame and layout
         frame = QFrame(self)
         frame.setStyleSheet("QWidget { background-color: #ffeaeaea }")
-        layout = QVBoxLayout()
         frame.setLayout(layout)
-        self.setCentralWidget(frame)
         
+        self.setCentralWidget(frame)
+    
         self._createActions()
         self._createMenuBar()
         # self._createToolBars()
         
         lexer = QsciLexerPython()
         lexer.setFont(QFont('Fira Code'))
-
-        # QScintilla editor setup
-        # ------------------------
-
-        # ! Make instance of QsciScintilla class!
-        editor = CustomEditor()
-
-        editor.setLexer(lexer)
-        editor.setUtf8(True)  # Set encoding to UTF-8
         
-        editor.setCaretLineVisible(True)
-        editor.setCaretLineBackgroundColor(QColor('gainsboro'))
-        
-        editor.setAutoIndent(True)
-        editor.setIndentationGuides(False)
-        editor.setIndentationsUseTabs(True)
-        editor.setIndentationWidth(4)
-        
-        editor.setMarginsBackgroundColor(QColor("gainsboro"))
-        editor.setMarginLineNumbers(1, True)
-        editor.setMarginWidth(1, 50)
-        
-        editor.setAutoCompletionSource(QsciScintilla.AutoCompletionSource.AcsAll)
-        editor.setAutoCompletionCaseSensitivity(True)
-        editor.setAutoCompletionReplaceWord(True);
-        editor.setAutoCompletionUseSingle(QsciScintilla.AutoCompletionUseSingle.AcusAlways);
-        editor.setAutoCompletionThreshold(0);
-
-        editor.setBraceMatching(QsciScintilla.BraceMatch.SloppyBraceMatch);
-        editor.setMatchedBraceBackgroundColor(QColor("white"));
-        editor.setUnmatchedBraceForegroundColor(QColor("blue"));
-        
-        self.editor = editor;
-            
-        layout.addWidget(editor)
+        self.editor = CustomEditor()
+        self.editor.setLexer(lexer)
+        self.editor.setVisible(False)
+        layout.addWidget(self.editor)
 
         self.show()
         
+    def closeEvent(self, event):
+        """
+        The closeEvent function is called when the user closes the window.
+        It checks if there are any unsaved changes in the file and prompts for confirmation before closing.
+        
+        :param event: PyQt automatically passes the event object to the function
+        """
+        if self.editor.unsaved_changes and not self.is_new_file:
+            self.saveFile(self.opened_file_path)
+        
+        if self.is_new_file:
+            reply = QMessageBox.question(self, 'Несохраненные изменения', 'Файл не был сохранен, вы действительно хотите выйти?',
+                    QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, QMessageBox.StandardButton.No)
+
+            if reply == QMessageBox.StandardButton.Yes:
+                event.accept()
+            else:
+                event.ignore()
         
     def _createActions(self):
-        newAction = QAction("&New", self)
-        newAction.setStatusTip("Create new file")
-        newAction.triggered.connect(self.newActionHandler)
+        """
+        The _createActions function creates actions for the menu bar.
+        """
+        self.newAction = QAction("&Создать новый файл", self)
+        self.newAction.setShortcut("Ctrl+n")
+        self.newAction.triggered.connect(self.newActionHandler)
 
-        openAction = QAction("&Open...", self)
-        openAction.setStatusTip("Open file")
-        openAction.triggered.connect(self.openActionHandler)
+        self.openAction = QAction("&Открыть файл...", self)
+        self.openAction.setShortcut("Ctrl+o")
+        self.openAction.triggered.connect(self.openActionHandler)
         
-        self.saveAction = QAction("&Save", self)
-        self.exitAction = QAction("&Exit", self)
-        self.copyAction = QAction("&Copy", self)
-        self.pasteAction = QAction("&Paste", self)
-        self.cutAction = QAction("C&ut", self)
-        self.helpContentAction = QAction("&Help Content", self)
-        self.aboutAction = QAction("&About", self)
+        self.saveAction = QAction("&Сохранить", self)
+        self.saveAction.setShortcut("Ctrl+s")
+        self.saveAction.triggered.connect(self.saveActionHandler)
         
-        self.newAction = newAction
-        self.openAction = openAction
+        self.saveAsAction = QAction("&Сохранить как...", self)
+        self.saveAsAction.setShortcut("Ctrl+Shift+s")
+        self.saveAsAction.triggered.connect(self.saveAsActionHandler)
+        
+        self.closeAction = QAction("&Закрыть файл", self)
+        self.closeAction.setShortcut("Ctrl+w")
+        self.closeAction.triggered.connect(self.closeActionHandler)
+        
+        self.exitAction = QAction("&Выйти", self)
+        self.copyAction = QAction("&Копировать", self)
+        self.pasteAction = QAction("&Вставить", self)
+        self.cutAction = QAction("&Вырезать", self)
+        self.helpContentAction = QAction("&Документация", self)
+        self.aboutAction = QAction("&О редакторе", self)
+
         
     def _createMenuBar(self):
+        """
+        The _createMenuBar function creates the menu bar for the application.
+        It adds actions to each of its menus, and sets icons where applicable.
+        """
         menuBar = self.menuBar()
-        # Creating menus using a QMenu object
-        fileMenu = QMenu("&File", self)
-        menuBar.addMenu(fileMenu)
+
+        fileMenu = menuBar.addMenu("&Файл")
         fileMenu.addAction(self.newAction)
         fileMenu.addAction(self.openAction)
         fileMenu.addAction(self.saveAction)
         fileMenu.addAction(self.exitAction)
-        # Edit menu
-        editMenu = menuBar.addMenu("&Edit")
+        fileMenu.addAction(self.closeAction)
+
+        editMenu = menuBar.addMenu("&Редактировать")
         editMenu.addAction(self.copyAction)
         editMenu.addAction(self.pasteAction)
         editMenu.addAction(self.cutAction)
-        # Help menu
-        helpMenu = menuBar.addMenu(QIcon(":help-content.svg"), "&Help")
+
+        helpMenu = menuBar.addMenu(QIcon(":help-content.svg"), "&Помощь")
         helpMenu.addAction(self.aboutAction)
         
         
-    def newActionHandler(self): pass
+    def newActionHandler(self):
+        """
+        The newActionHandler function creates a new file in the editor.
+        """
+        self.editor.setVisible(True)
+        self.editor.setText("")
+        
+        self.is_new_file = True
+    
     def openActionHandler(self):
-        file_name = self.get_file()
-        with open(file_name) as f:
-            self.editor.setText(f.read())
+        """
+        The openActionHandler function opens a file dialog and sets the text of the editor to the contents of 
+        the chosen file. If no file is chosen, nothing happens.
+        """
+        file_path, _ = self.choose_file()
+        if file_path:
+            with open(file_path, 'r', encoding='utf8') as f:
+                self.opened_file_path = file_path
+                self.is_new_file = False
+                self.editor.setVisible(True)
+                self.editor.setText(f.read())
+                
+    def saveFile(self, file_path):
+        """
+        The saveFile function saves the contents of the editor to a file.
+        It does this by writing to an open file object, which is created with
+        the 'w' flag (indicating that it is opened for writing). The function 
+        also sets self.unsaved_changes to False and updates self.opened_file_path 
+        to reflect the new path of the saved file.
+        
+        :param file_path: Save the file to a specific location
+        """
+        with open(file_path, 'w', encoding='utf8') as f:
+            text = self.editor.text()
+            text = text.replace('\r', '')
+            f.write(text)
+                
+            if self.is_new_file:
+                self.opened_file_path = file_path
+                self.is_new_file = False
+                
+            self.unsaved_changes = False
             
-    def saveActionHandler(self): pass
-    def exitActionHandler(self): pass
-    
-    def copyActionHandler(self): pass
-    def pasteActionHandler(self): pass
-    def cutActionHandler(self): pass
-    
-    def aboutActionHandler(self): pass
+    def saveActionHandler(self): 
+        """
+        The saveActionHandler method saves the current file to a specified location.
+        If no location is specified, it will save the file to its original location.
+        """
+        if self.is_new_file:
+            file_path, _ = self.choose_file_save()
+        else:
+            file_path = self.opened_file_path
+            
+        if file_path:
+            self.saveFile(file_path)
+            
+    def saveAsActionHandler(self):
+        """
+        The saveAsActionHandler method is called when the user selects the saveAsAction from the file menu. 
+        It opens a dialog box to allow them to choose where they want to save their file and what name they want it saved as. 
+        If a path is chosen, then that path is used in self.saveFile(file_path) which saves the current document.
+        """
+        file_path, _ = self.choose_file_save()
 
-    def get_file(self):
-        return QFileDialog.getOpenFileName(self, 'Open file', __file__)[0] or __file__
+        if file_path:
+            self.saveFile(file_path)
+            
+    def closeActionHandler(self):
+        """
+        The closeActionHandler function clears the editor and makes it invisible when file closes.
+        """
+        self.editor.clear()
+        self.editor.setVisible(False)
     
+    # TODO: implement cringe usless functions
+    def exitActionHandler(self): raise NotImplementedError
+    
+    def copyActionHandler(self): raise NotImplementedError
+    def pasteActionHandler(self): raise NotImplementedError
+    def cutActionHandler(self): raise NotImplementedError
+    
+    def aboutActionHandler(self): raise NotImplementedError
+
+    def choose_file(self) -> Tuple[str, str]:
+        """
+        The choose_file function allows the user to select a file from their computer.
+        The function returns a tuple containing the path and filter used to choose the file.
+        
+        `path, filters = self.choose_file()`
+        :return: A tuple containing the path and filter
+        """
+        return QFileDialog.getOpenFileName(self, 'Open file', __file__) or __file__
+    
+    def choose_file_save(self) -> Tuple[str, str]:
+        return QFileDialog.getSaveFileName(self, 'Save File')
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
